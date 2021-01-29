95459be3133c6570a3938a813630d637b763b954
Ethan-Chiu <ethanchiu0608@gmail.com>
2021-01-30 00:35:13 +0800

    shader load from asset file

--- EthaneEngine/src/Ethane/Renderer/Shader.cpp
+++ EthaneEngine/src/Ethane/Renderer/Shader.cpp
@@ -6,6 +6,21 @@
 
 namespace Ethane {
 
+	Shader* Shader::Create(const std::string& filepath)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			ETH_CORE_ASSERT(false, "endererAPI::None is currently not supported!");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return new OpenGLShader(filepath);
+		}
+
+		ETH_CORE_ASSERT(false, "Unknown RendererAPI");
+		return nullptr;
+	}
+
 	Shader* Shader::Create(const std::string& vertexSrc, const std::string& fragmentSrc)
 	{
 		switch (Renderer::GetAPI())
