b66fb6b5341d0e051be484e30f62de5dd23a2bec
Ethan-Chiu <ethanchiu0608@gmail.com>
2021-02-01 13:29:03 +0800

    Resizing

--- EthaneEngine/src/Ethane/Application.cpp
+++ EthaneEngine/src/Ethane/Application.cpp
@@ -49,6 +49,7 @@ namespace Ethane
 	{
 		EventDispatcher dispatcher(e);
 		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FUNCTION(OnWindowClose));
+		dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FUNCTION(OnWindowResize));
 
 		for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
 		{
@@ -66,11 +67,15 @@ namespace Ethane
 			Timestep timestep = time - m_LastFrameTime;
 			m_LastFrameTime = time;
 
-			for (Layer* layer : m_LayerStack)
-			   layer->OnUpdate(timestep);
+			if (!m_Minimized)
+			{
+				for (Layer* layer : m_LayerStack)
+					layer->OnUpdate(timestep);
+			}
 
 			// auto [x, y] = Input::GetMousePosition();
 			// ETH_CORE_TRACE("{0}, {1}", x, y);
+
 			m_ImGuiLayer->Begin();
 			for (Layer* layer : m_LayerStack)
 				layer->OnImGuiRender();
@@ -84,5 +89,19 @@ namespace Ethane
 	{
 		m_Running = false;
 		return true;
+	}
+
+	bool Application::OnWindowResize(WindowResizeEvent& e)
+	{
+		if (e.GetWidth() == 0 || e.GetHeight() == 0)
+		{
+			m_Minimized = true;
+			return false;
+		}
+
+		m_Minimized = false;
+		Renderer::OnWindowResize(e.GetWidth(), e.GetHeight());
+
+		return false;
 	}
 }
