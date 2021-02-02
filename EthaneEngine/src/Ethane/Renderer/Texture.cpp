8554b323ad5f796058d122e0f5e7fc87d5ed1018
Ethan-Chiu <ethanchiu0608@gmail.com>
2021-02-02 20:07:49 +0800

    2D Renderer Single Shader

--- EthaneEngine/src/Ethane/Renderer/Texture.cpp
+++ EthaneEngine/src/Ethane/Renderer/Texture.cpp
@@ -6,6 +6,21 @@
 
 namespace Ethane {
 
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			ETH_CORE_ASSERT(false, "endererAPI::None is currently not supported!");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height);
+		}
+
+		ETH_CORE_ASSERT(false, "Unknown RendererAPI");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
@@ -14,7 +29,7 @@ namespace Ethane {
 			ETH_CORE_ASSERT(false, "endererAPI::None is currently not supported!");
 			return nullptr;
 		case RendererAPI::API::OpenGL:
-			return std::make_shared<OpenGLTexture2D>(path);
+			return CreateRef<OpenGLTexture2D>(path);
 		}
 
 		ETH_CORE_ASSERT(false, "Unknown RendererAPI");
