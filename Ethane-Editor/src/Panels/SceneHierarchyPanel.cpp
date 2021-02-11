d9054d75da527b69d9aee4c1ae1915ab1f7403aa
Ethan-Chiu <ethanchiu0608@gmail.com>
2021-02-11 23:39:24 +0800

    Properties Panel

--- Ethane-Editor/src/Panels/SceneHierarchyPanel.cpp
+++ Ethane-Editor/src/Panels/SceneHierarchyPanel.cpp
@@ -2,6 +2,7 @@
 #include "Ethane/Scene/Components.h"
 
 #include <imgui/imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Ethane {
 
@@ -26,6 +27,14 @@ namespace Ethane {
 			DrawEntityNode(entity);
 		});
 
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+			m_SelectionContext = {};
+
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponent(m_SelectionContext);
 		ImGui::End();
 	}
 
@@ -40,6 +49,33 @@ namespace Ethane {
 		if (opened)
 		{
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHierarchyPanel::DrawComponent(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+			
+			char buffer[256];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+				ImGui::TreePop();
+			}
 		}
 	}
 
--- Ethane-Editor/src/Panels/SceneHierarchyPanel.h
+++ Ethane-Editor/src/Panels/SceneHierarchyPanel.h
@@ -18,6 +18,7 @@ namespace Ethane {
 		void OnImGuiRender();
 	private:
 		void DrawEntityNode(Entity entity);
+		void DrawComponent(Entity entity);
 	private:
 		Ref<Scene> m_Context;
 		Entity m_SelectionContext;
