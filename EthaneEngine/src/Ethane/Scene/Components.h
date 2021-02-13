ca2ec6ab1971592b261b6be8607643d1945c9883
Ethan-Chiu <ethanchiu0608@gmail.com>
2021-02-13 13:06:12 +0800

    Change Transform Component

--- EthaneEngine/src/Ethane/Scene/Components.h
+++ EthaneEngine/src/Ethane/Scene/Components.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
 
 #include "SceneCamera.h"
 #include "ScriptableEntity.h"
@@ -19,15 +20,21 @@ namespace Ethane {
 
 	struct TransformComponent
 	{
-		glm::mat4 Transform{ 1.0f };
+		glm::vec3 Translation = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 Rotation = { 0.0f, 0.0f, 0.0f };
+		glm::vec3 Scale = { 1.0f, 1.0f, 1.0f };
 
 		TransformComponent() = default;
 		TransformComponent(const TransformComponent&) = default;
-		TransformComponent(const glm::mat4& transform)
-			:Transform(transform) {}
-
-		operator const glm::mat4& () const { return Transform; }
-		operator glm::mat4& () { return Transform; }
+		TransformComponent(const glm::vec3& translation)
+			:Translation(translation) {}
+		
+		glm::mat4 GetTransform() const
+		{
+			glm::mat4 rotation = glm::rotate(glm::rotate(glm::rotate(glm::mat4(1.0f), Rotation.x, { 1, 0, 0 }), Rotation.y, { 0, 1, 0 }), Rotation.z, { 0, 0, 1 });
+			
+			return glm::scale(glm::translate(glm::mat4(1.0f), Translation) * rotation, Scale);
+		}
 	};
 
 	struct SpriteRendererComponent
