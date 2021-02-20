55105e5d2a811df39b560f568472736575c4c9d4
ethanchiu0608@gmail.com <ethanchiu0608@gmail.com>
2021-02-20 14:39:30 +0800

    Clearing Framebuffer Texture Attachment

--- EthaneEngine/src/Platform/OpenGL/OpenGLFramebuffer.cpp
+++ EthaneEngine/src/Platform/OpenGL/OpenGLFramebuffer.cpp
@@ -75,6 +75,19 @@ namespace Ethane {
 
 			return false;
 		}
+
+		static GLenum EthaneFBTextureFormatToGL(FramebufferTextureFormat format)
+		{
+			switch (format)
+			{
+			case FramebufferTextureFormat::RGBA8:       return GL_RGBA8;
+			case FramebufferTextureFormat::RED_INTEGER: return GL_RED_INTEGER;
+			}
+
+			ETH_CORE_ASSERT(false);
+			return 0;
+		}
+
 	}
 
 	OpenGLFramebuffer::OpenGLFramebuffer(const FramebufferSpecification& spec)
@@ -193,6 +206,14 @@ namespace Ethane {
 		int pixelData;
 		glReadPixels(x, y, 1, 1, GL_RED_INTEGER, GL_INT, &pixelData);
 		return pixelData;
+	}
+
+	void OpenGLFramebuffer::ClearAttachment(uint32_t attachmentIndex, int value)
+	{
+		ETH_CORE_ASSERT(attachmentIndex < m_ColorAttachments.size());
+
+		auto& spec = m_ColorAttachmentSpecifications[attachmentIndex];
+		glClearTexImage(m_ColorAttachments[attachmentIndex], 0, Utils::EthaneFBTextureFormatToGL(spec.TextureFormat), GL_INT, &value);
 	}
 
 }
